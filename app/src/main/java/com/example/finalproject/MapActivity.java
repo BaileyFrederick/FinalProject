40562340fb262b2333012c2d7e36bc574b4203b6
ashleys20 <ashley20@live.unc.edu>
2019-04-18 00:34:24 -0400

    updated health activity features

--- app/src/main/java/com/example/finalproject/MapActivity.java
+++ app/src/main/java/com/example/finalproject/MapActivity.java
@@ -1,8 +1,10 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    public void backToMain(View v){
+        Intent x = new Intent(this, MainActivity.class);
+        startActivity(x);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outstate){
+
+
+
+        super.onSaveInstanceState(outstate);
+
     }
 }
