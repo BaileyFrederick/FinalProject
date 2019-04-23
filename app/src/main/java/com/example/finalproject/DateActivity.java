774bccc99d7280adbbd718a4781d769e8d44cd71
Bailey Frederick <FrederickBailey18@gmail.com>
2019-04-22 23:54:44 -0400

    Calendar

--- app/src/main/java/com/example/finalproject/DateActivity.java
+++ app/src/main/java/com/example/finalproject/DateActivity.java
@@ -3,6 +3,7 @@ package com.example.finalproject;
 import android.content.Context;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -13,10 +14,15 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -31,10 +37,12 @@ public class DateActivity extends AppCompatActivity {
     FrameLayout l;
     LayoutInflater inflater;
     View customView;
-    FirebaseHandler f = new FirebaseHandler();
+    private FirebaseDatabase mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mDatabase = FirebaseDatabase.getInstance();
         setContentView(R.layout.activity_date);
         Bundle bundle = getIntent().getExtras();
         Date d = (Date) bundle.get("Date");
@@ -45,7 +53,22 @@ public class DateActivity extends AppCompatActivity {
         TextView dayText = (TextView) findViewById(R.id.dayTV);
         dayText.setText(day);
 
-        setUp(date);
+        final DatabaseReference myRef = mDatabase.getReference("Calendar");
+        final List<Event> list = new ArrayList<Event>();
+        myRef.child(date).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
+                    Event e = noteDataSnapshot.getValue(Event.class);
+                    addEventView(e);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
         l  = findViewById(R.id.main);
@@ -61,7 +84,7 @@ public class DateActivity extends AppCompatActivity {
         //add view
 
         if(!open) {
-            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 1200);
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 1500);
             //top shows time
             //height duration
             params.setMargins(((width / 2) - 400), 194, 0, 0);
@@ -76,13 +99,11 @@ public class DateActivity extends AppCompatActivity {
     }
 
     public void setUp(String d){
-        List<Event> e = f.getEvents(d);
-        for(int i =0; i < e.size(); i++){
-            addEventView(e.get(i));
-        }
+
     }
 
     public void addEventView(Event event){
+
         //add views for each one at specific time
         display = getWindowManager(). getDefaultDisplay();
         Point size = new Point();
@@ -92,11 +113,38 @@ public class DateActivity extends AppCompatActivity {
         v = findViewById(R.id.fl);
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         final View customView = inflater.inflate(R.layout.activity_custom, null);
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(750,300);
+        double dur = ((double)(event.duration/60))*190;
+
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(750,190);
         //top shows time
         //height duration
-        params.setMargins(75, 0, 0, 0);
+        int time = Integer.parseInt(event.time.replace(":",""));
+        double t = ((double)time)/100;
+        t = t*190;
+        t = t+30;
+        if(time>=300 && time<1000){
+            t = t+10;
+        }else if(time>=1000 && time<1200){
+            t = t+20;
+        }else if(time>=1200 && time<1700){
+            t = t+30;
+        }else if(time>=1700 && time<1900){
+            t = t+40;
+        }else if(time>=1900 && time<2400){
+            t = t+50;
+        }
+
+
+        params.setMargins(75, (int)t, 0, 0);
         customView.setLayoutParams(params);
+        TextView e = (TextView) findViewById(R.id.eventTV);
+        TextView l = (TextView) findViewById(R.id.locTV);
+        TextView te = (TextView) findViewById(R.id.timeTV);
+        TextView d = (TextView) findViewById(R.id.durTV);
+        e.setText(event.desc);
+        l.setText(event.loc);
+        te.setText(event.time);
+        d.setText(event.duration);
         v.addView(customView);
     }
 
